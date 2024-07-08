d5a863b2d1f0baaa70d3c85d785dfd63184b043e
tsubasa2830127 <tsubasawosazukeru283@gmail.com>
2024-07-08 10:51:10 +0900

    Create TangibleAsset.java

---
+++ TangibleAsset.java
@@ -0,0 +1,23 @@
+public abstract class TnagibleAsset {
+  private String name;
+  private int price;
+  private String color;
+
+ public TangibleAsset(String name, int price, String golor) {
+   this.name = name;
+   this.price = price;
+   this.color = color;
+ }
+
+ public String getName() {
+   returun this.name;
+ }
+
+ public int getPrice() {
+   returun this.price;
+ }
+
+ public String getColor() {
+   returun this.Color;
+ }
+}
